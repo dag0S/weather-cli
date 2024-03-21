7cdf48d7d9d2ea45feab1d785aa17b3940d27f1d
Gosudarev Danila <danidagosudarev@gmail.com>
2024-03-21 21:34:42 +0300

    os && path

--- weather.js
+++ weather.js
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
   console.log(args);
+  // Вывести помощь
   if (args.h) {
     printHelp();
   }
+  // Сохранить город
   if (args.s) {
-    // Сохранить город
   }
+  // Созранить токен
   if (args.t) {
-    // Созранить токен
+    saveKeyValue('token', args.t)
   }
   // Вывести погоду
 };
